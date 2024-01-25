81fe47e6ee9563a4daeb7a1f5899071d92de09d0
platinum1129 <platinum1129@hotmail.com>
2024-01-26 02:22:14 +0900

    index.html作成

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ハッカソン用プロジェクト
 構成参考：https://qiita.com/Bashi50/items/30065e8f54f7e8038323
+
+# 起動方法
+flask/server.py を実行
+
+## 起動URL
+http://127.0.0.1:5000/
+
--- flask/quizapp/views.py
+++ flask/quizapp/views.py
@@ -1,5 +1,11 @@
+from flask import render_template
 from quizapp import app
 
 @app.route('/')
 def index():
-    return 'Hellow World!'
+    my_dict = {
+        'insert_something1': 'views.pyのinsert_something1部分です。',
+        'insert_something2': 'views.pyのinsert_something2部分です。',
+        'test_titles': ['title1', 'title2', 'title3']
+    }
+    return render_template('pages/index.html', my_dict=my_dict)
